b3e499614433baab0a0acbd7a169a12d236a69a4
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:40:06 +0530

    Create Numbers from 1 to 100.c

---
+++ Numbers from 1 to 100.c
@@ -0,0 +1,37 @@
+#include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int i;  
+
+  
+
+          for(i=1;i<=100;i++)  
+
+  
+
+          {  
+
+  
+
+                  printf("%d\n",i);  
+
+  
+
+          }  
+
+  
+
+          return 0;  
+
+  
+
+  }
