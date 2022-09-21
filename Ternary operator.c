506ac74f6740e265819179f0219cd8f64201d69e
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:55:07 +0530

    Create Ternary operator.c

---
+++ Ternary operator.c
@@ -0,0 +1,41 @@
+//program in c using ternary operator  
+
+  
+
+  #include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int a,b,c;  
+
+  
+
+          printf("please enter two numbers");  
+
+  
+
+          scanf("%d%d",&a,&b);  
+
+  
+
+          c=a>b?a:b;  
+
+  
+
+          printf("%d is greatest",c);  
+
+  
+
+            
+
+  
+
+  }
