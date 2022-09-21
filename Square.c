0fce80461e9edaf43232878d32fcdff9cfb13fd8
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:52:43 +0530

    Create Square.c

---
+++ Square.c
@@ -0,0 +1,53 @@
+#include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int i,n;  
+
+  
+
+          for(i=1;i<=3;i++)  
+
+  
+
+               {  
+
+  
+
+                       for(n=1;n<=4;n++)  
+
+  
+
+                       {  
+
+  
+
+                               printf("*  ");  
+
+  
+
+                       }  
+
+  
+
+                 printf("\n");  
+
+  
+
+               }  
+
+  
+
+               return 0;  
+
+  
+
+  }
