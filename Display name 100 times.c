a90a2319f62dce521d4e69dc334b693d1c2288af
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:28:58 +0530

    Create Display name 100 times.c

---
+++ Display name 100 times.c
@@ -0,0 +1,37 @@
+#include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int i;  
+
+  
+
+          for(i=1;i<=100;i++)  
+
+  
+
+          {  
+
+  
+
+                  printf("Basit Aziz \t");  
+
+  
+
+          }  
+
+  
+
+          return 0;  
+
+  
+
+  }
