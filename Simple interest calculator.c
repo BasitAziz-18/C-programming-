133d8103825f3c1b2a3706001bcc5f6de4e4532c
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:52:12 +0530

    Create Simple interest calculator.c

---
+++ Simple interest calculator.c
@@ -0,0 +1,49 @@
+//program in c to calculate simple interest  
+
+  
+
+  #include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int p,n,r,si;  
+
+  
+
+          p=5000;  
+
+  
+
+          n=1;  
+
+  
+
+          r=7;  
+
+  
+
+          //formula of simple interest  
+
+  
+
+          si= p * n * r/100;  
+
+  
+
+          printf("%d\t",si);  
+
+  
+
+          return 0;  
+
+  
+
+  }
