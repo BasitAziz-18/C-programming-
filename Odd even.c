7a669202547040076636ad1ad48c01c56011c5aa
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:40:41 +0530

    Create Odd even.c

---
+++ Odd even.c
@@ -0,0 +1,45 @@
+#include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int a;  
+
+  
+
+          printf("Please enter any Number:\n");  
+
+  
+
+          scanf("%d",&a);  
+
+  
+
+          if(a%2 == 0)  
+
+  
+
+          printf("%d is even",a);  
+
+  
+
+          else  
+
+  
+
+          printf("%d is odd",a);  
+
+  
+
+  return 0;          
+
+  
+
+  }
