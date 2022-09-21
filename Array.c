a6bd1a44dcc15c24261c1c3fb709070002778982
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:26:07 +0530

    Create Array.c

---
+++ Array.c
@@ -0,0 +1,41 @@
+#include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int i,a[5];  
+
+  
+
+          printf("please enter array elements\t");  
+
+  
+
+          for(i=0;i<=4;i++)  
+
+  
+
+          {  
+
+  
+
+          scanf("%d",&a[i]);  
+
+  
+
+          }  
+
+  
+
+                  printf("%d\t",a[i]);  
+
+  
+
+  }
