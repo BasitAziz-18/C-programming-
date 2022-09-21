fcc83d8337e836a9e93099267c6c4cb488be7173
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:13:55 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,39 @@
+#include<stdio.h>  
+
+  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int a,b,sum;  
+
+  
+
+          printf("please enter two integers\n");  
+
+  
+
+          scanf("%d%d",&a,&b);  
+
+  
+
+          sum=a+b;  
+
+  
+
+          printf("sum is %d",sum);  
+
+  
+
+  return 0;          
+
+  
+
+  }
