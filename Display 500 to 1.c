4b529460d576c9615106ec8ed13fc6fd7a5415b8
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:29:50 +0530

    Create Display 500 to 1.c

---
+++ Display 500 to 1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int i;  
+
+  
+
+          for(i=500;i>=1;i--)  
+
+  
+
+          {  
+
+  
+
+                  printf("%d\n",i);  
+
+  
+
+          }  
+
+  
+
+          return 0;  
+
+  
+
+  }
