0ae2bce0b351242df8fe0e7a53d728de7450e908
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:44:39 +0530

    Create Pyramid.c

---
+++ Pyramid.c
@@ -0,0 +1,53 @@
+#include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int i,n;  
+
+  
+
+          for(i=1;i<=6;i++)  
+
+  
+
+               {  
+
+  
+
+                       for(n=1;n<=i;n++)  
+
+  
+
+                       {  
+
+  
+
+                               printf("*  ");  
+
+  
+
+                       }  
+
+  
+
+                 printf("\n");  
+
+  
+
+               }  
+
+  
+
+               return 0;  
+
+  
+
+  }
