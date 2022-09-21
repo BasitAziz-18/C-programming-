8d887dcbc57ef4b790bd7cfd6dded6753a10b551
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2022-09-21 22:54:33 +0530

    Create Sum of ist n natural numbers.c

---
+++ Sum of ist n natural numbers.c
@@ -0,0 +1,53 @@
+#include<stdio.h>  
+
+  
+
+  int main()  
+
+  
+
+  {  
+
+  
+
+          int i,n,sum;  
+
+  
+
+          printf("please enter value of n\t");  
+
+  
+
+          scanf("%d",&n);  
+
+  
+
+          sum=0;  
+
+  
+
+          for(i=1;i<=n;i++)  
+
+  
+
+               {  
+
+  
+
+                       sum=sum+i;  
+
+  
+
+               }  
+
+  
+
+               printf("the  sum = %d",sum);  
+
+  
+
+           return 0;  
+
+  
+
+  }
